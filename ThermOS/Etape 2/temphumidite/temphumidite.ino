b072f85a3e5d460baec4cc67312880029d63d607
CThomas <corde.thomas1@gmail.com>
2025-10-21 14:36:40 +0200

    modif

--- ThermOS/Etape 2/temphumidite/temphumidite.ino
+++ ThermOS/Etape 2/temphumidite/temphumidite.ino
@@ -1,3 +1,5 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,  16, 2);
 #include "DHT.h"
 #define DHTPIN 2
@@ -5,11 +7,27 @@ LiquidCrystal_I2C lcd(0x27,  16, 2);
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(3 OUTPUT)
+  pinMode(4 OUTPUT)
+  pinMode(5 OUTPUT)
+  
   dht.begin();
     //initialize lcd screen
 }
 
 void loop() {
+  if (temperature < 18)
+  digitaleWrite(2, OUTPUT);
+  digitaleWrite(4, LOW);
+  digitaleWrite(5, LOW);
 
+
+
+
+
+  // Recupere la temperature et l'humidite du capteur et l'affiche
+  // sur le moniteur serie
+  Serial.println("Temperature = " + String(dht.readTemperature())+" °C");
+  // Attend 10 secondes avant de reboucler
+  delay(10000);
 }
